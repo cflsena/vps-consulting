2c2523f69d4d850c88d7eb230383dcaa63338ce6
Cleiton Sena <cflsena2@gmail.com>
2026-06-21 20:22:48 -0300

    chore: inicializar projeto Spring Boot com configurações e dependências

---
+++ java/src/main/java/br/com/vps/consulting/b2b/management/B2BManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.vps.consulting.b2b.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2BManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2BManagementApplication.class, args);
+	}
+
+}
